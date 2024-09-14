767204445944ad2d6001170562ac25ed28f058e7
Virtus <virtuso@gmail.com>
2024-09-14 11:46:16 +0200

    Base

--- migrations/env.py
+++ migrations/env.py
@@ -1,17 +1,12 @@
-import os
-import sys
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 
-sys.path.append(os.path.join(sys.path[0], 'src'))
-
-from src.auth.models import *
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.database import metadata, Base
-from src.operations.models import *
-from src.chat.models import *
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from models.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -33,7 +28,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata, Base.metadata]
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
